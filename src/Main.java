e527e37585fddcd29153baf0c118352c92783b89
Hincu Iulian <iulian.hincu801@gmail.com>
2025-03-10 21:20:26 +0200

    Am adaugat clase noi

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+      Scanner scanner = new Scanner(System.in); // Am creat un obiect scanner
+        System.out.println("Adauga un numar:");
+        int numar = scanner.nextInt(); // Citim un numar de la tastatura
+
+        // Verificarea daca numarul este par sau impar
+        if (numar % 2 == 0) {
+            System.out.println(numar + " este par");
+        } else {
+            System.out.println(numar + " este impar");
+        }
     }
 }
+
+
---
+++ src/testare/HelloWorldTekwill.java
@@ -0,0 +1,9 @@
+package testare;
+
+import java.util.Scanner;
+
+public class HelloWorldTekwill {
+    public static void main(String[] args) {
+        Scanner role = new Scanner(System.in);
+    }
+}

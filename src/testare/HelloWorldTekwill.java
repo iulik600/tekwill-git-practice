1aa434f8b9e11d0f7323280cd303ae042227afcc
Hincu Iulian <iulian.hincu801@gmail.com>
2025-03-13 19:20:29 +0200

    Am adaugat clase noi

--- src/testare/HelloWorldTekwill.java
+++ src/testare/HelloWorldTekwill.java
@@ -4,6 +4,38 @@ import java.util.Scanner;
 
 public class HelloWorldTekwill {
     public static void main(String[] args) {
-        Scanner role = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Introduceti codul valutar(EUR, GBP, INR, AUD: ");
+        String moneda = scanner.next();
+
+        System.out.println("Introduceti valoarea in USD: ");
+        double sumaUSD = scanner.nextDouble();
+
+        double sumaConvertita = 0;
+        boolean monedaValida = true;
+
+        switch (moneda) {
+            case "EUR":
+                sumaConvertita = sumaUSD * 0.85;
+                break;
+            case "GBP":
+                sumaConvertita = sumaUSD * 0.75;
+                break;
+            case "INR":
+                sumaConvertita = sumaUSD * 75.0;
+                break;
+            case "AUD":
+                sumaConvertita = sumaUSD * 1.35;
+                break;
+            default:
+                System.out.println("Valuta nu este acceptata");
+        }
+
+        if(monedaValida) {
+            System.out.println("Suma convertita: " +  sumaConvertita + moneda);
+        }
+
+        scanner.close();
+
     }
 }
